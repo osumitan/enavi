018b6f8680cbfc61ccf3a68c13f5b101eda22f92
osumitan <osumitan@gmail.com>
2017-03-04 09:32:40 +0900

    arealist対応

--- enavi.sh
+++ enavi.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 # ホームフォルダ
 HOME=~/max3tb/data/develop/sh/enavi
+export HOME
 
 # ログフォルダ
 DT=`date +"%Y%m%d%H%M%S"`
+export DT
 LOGDIR="${HOME}/log"
+export LOGDIR
 
 # 処理
-proc() {
+function proc() {
+	# 空行無視
+	if [ "$1" = "" ]
+	then
+		return
+	fi
+	
 	# ID
 	ID=$1
 	echo "■ ■ ■ ID=${ID} ■ ■ ■"
@@ -31,10 +40,10 @@ proc() {
 	echo "■ ■ ■ クリーン ■ ■ ■"
 	ls -r ${LOGDIR}/*${ID}* | gawk 'BEGIN{a=0;}{if(a>=2){printf("rm -f %s\n",$0);}a++;}' | bash
 }
+export -f proc
 
-# 千葉エリア
-proc "0500"
+#proc "0500"
 # 船橋エリア
-proc "0510"
-
+#proc "0510"
+cat ${HOME}/arealist.txt | egrep -v "^#" | xargs -I % bash -c "proc %"
 
